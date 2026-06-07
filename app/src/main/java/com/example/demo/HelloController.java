feacf390a188962301621b72ebf059c6c7a505a9
hasnaoui <ayoub.hasnaoui@aderivatives.com>
2026-06-07 17:34:40 +0100

    v15 version

--- app/src/main/java/com/example/demo/HelloController.java
+++ app/src/main/java/com/example/demo/HelloController.java
@@ -12,14 +12,14 @@ public class HelloController {
     @GetMapping("/")
     public Map<String, String> home() {
         return Map.of(
-                "message", "Hello Ayoub, GitOps upadted to v10",
-                "version", "v10",
+                "message", "Hello Ayoub, GitOps upadted to v15",
+                "version", "v15",
                 "time", Instant.now().toString()
         );
     }
 
     @GetMapping("/hello")
     public String hello() {
-        return "Hello GitOps from Spring Boot v10";
+        return "Hello GitOps from Spring Boot v15";
     }
 }
